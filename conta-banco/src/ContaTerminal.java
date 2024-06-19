9980ddcd13c982cc35928d3586ae3d2ee467f8bb
Arnaldo dutra <arnaldodsj@gmail.com>
2024-06-19 11:27:45 -0300

    Conta bancaria criada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //  Exibir mensagens para os nossos usuários
+        System.out.println("Bem-vindo ao Banco Digital!");
+        
+        // Obter os valores digitados no terminal
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+        
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        
+        // Limpar o caractere de nova linha do buffer
+        scanner.nextLine();
+        
+        System.out.print("Por favor, digite o nome do Titular: ");
+        String titular = scanner.nextLine();
+        
+        System.out.print("Por favor, digite o saldo inicial: ");
+        double saldo = scanner.nextDouble();
+        
+        // Exibir a mensagem conta criada
+        System.out.println("\nConta criada com sucesso!");
+        System.out.println("Agência: " + agencia);
+        System.out.println("Número: " + numero);
+        System.out.println("Titular: " + titular);
+        System.out.println("Saldo: R$ " + saldo);
+        
+        // Fechar o scanner para evitar vazamento de recursos
+        scanner.close();
     }
 }
